8657164d3020cd01b607ee167d6b5b6fde8cfa25
Matt Kiernan <kiernan@mail.usf.edu>
2017-08-11 14:02:45 -0400

    added global parameter swift file

---
+++ iOS Chromecast/GlobalParams.swift
@@ -0,0 +1,40 @@
+//
+//  Global Variables.swift
+//  Nielsen Cloud App
+//
+//  Created by Nielsen on 4/9/17.
+//  Copyright © 2017 Nielsen. All rights reserved.
+//
+
+import Foundation
+
+// MARK: App Global
+var apid = "TF250E505-4A84-10CF-E040-070AAB3117C2"
+var devid = "SAMPLE12345643"
+var apn = "Kiernan iOS Chromecast Sample"
+var apv = "1.0"
+var uoo = "false"
+var sessionID = ""
+
+// MARK: Metadata
+var assetid = "Assetid123"
+var program = "Program123"
+var title = "Title123"
+var segB = "SegB123"
+var segC = "SegC321"
+var airdate = "20151231 18:30:00"
+var isfullepisode = "n"
+var crossId1 = "CrossId1xxx"
+var crossId2 = ""
+var adloadtype = "2"
+var hasAds = "0"
+var progen = "na"
+var length = "0"
+
+
+
+
+//Array setup
+var metadataValuesArray = [assetid, program, title, segB, segC, airdate, isfullepisode, crossId1, crossId2, adloadtype, hasAds, progen, uoo]
+
+var metadataNamesArray = ["assetid", "program", "title", "segB", "segC", "airdate", "isfullepisode", "crossId1", "crossId2", "adloadtype", "hasAds", "progen", "false"]
